34d0cf65a684d12e5255f98577ed43d366bb1024
jz435 <chopsticksmemset@gmail.com>
2024-04-25 16:22:44 +1200

    implement nextGeneration()

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -46,8 +46,35 @@ void print_board(bool *board, int board_size, int range){
     
 }
 
-
+/**
+*Implemention of the CPU version
+*Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+*Any live cell with two or three live neighbours lives on to the next generation.
+*Any live cell with more than three live neighbours dies, as if by overpopulation.
+*Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*Note: you can ignore the cells on four edges by setting row or col = 1 and row or col < board_size-1
+*/
 void nextGeneration(bool* board, bool* next_board, int board_size){
+    int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+    int dy[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+  
+    for (int row = 1; row < board_size - 1; row++) {
+        for (int col = 1; col < board_size - 1; col++) {
+            int live_neighbors = 0;
+            for (int i = 0; i < 8; i++) {
+                int new_row = row + dy[i];
+                int new_col = col + dx[i];
+                live_neighbors += board[new_col + new_row * board_size];
+            }
+            bool current_cell = board[col + row * board_size];
+            if (current_cell && (live_neighbors < 2 || live_neighbors > 3))
+                next_board[col + row * board_size] = false;
+            else if (!current_cell && live_neighbors == 3)
+                next_board[col + row * board_size] = true;
+            else
+                next_board[col + row * board_size] = current_cell;
+        }
+    }
 }
 
 int main()
